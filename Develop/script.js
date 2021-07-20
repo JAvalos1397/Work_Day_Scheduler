1191eb46802986c838ab5381ce6f2773db847221
Jose Avalos <avaljose@hotmai.com>
2021-07-19 22:48:37 -0500

    finished color coding

--- Develop/script.js
+++ Develop/script.js
@@ -9,40 +9,60 @@ function displayTime() {
   
 }
 
+//save feature
+var upper = $('.textArea');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function hour(){
     today = moment().format("H");
     time = parseInt(today);
     return time
-
 }
-
-
 
 function currtentTime() {
     var timeBlocks= $(".colorCode")
     // console.log(timeBlocks)
     for (let i = 0; i < timeBlocks.length ; i++) {
         index = parseInt(timeBlocks[i].id);
+        // timeID = "#" +timeBlocks[i].id  
         if (index < hour()) {
-            console.log(index)
-            index.addClass('past');
-        }
-
-
-        
+            // console.log("past", index)
+            $(timeBlocks[i]).addClass('past');
+        } else if (index === hour()) {
+            // console.log("present", index);
+            $(timeBlocks[i]).addClass('present');
+        } else if (index > hour()) {
+            console.log("future", index)
+            // $("#12").addClass('future')
+            $(timeBlocks[i]).addClass('future');
+        }        
     } 
-
 }    
 
 currtentTime()
 
 //updating time
 setInterval(function () {
-    // test()
     displayTime();
-    // minute();
     hour();
-    // currtentTime();
+    currtentTime();
 
 }, 1000);
