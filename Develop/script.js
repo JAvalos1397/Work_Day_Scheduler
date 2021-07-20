333d5501918ad702d83050b316ab7a43bb768677
Jose Avalos <avaljose@hotmai.com>
2021-07-19 19:00:01 -0500

    fixing timeblock

--- Develop/script.js
+++ Develop/script.js
@@ -9,12 +9,6 @@ function displayTime() {
   
 }
 
-function minute() {
-    today = moment().format("m");
-    time = parseInt(today);   
-    return time
-}
-
 
 function hour(){
     today = moment().format("H");
@@ -24,70 +18,31 @@ function hour(){
 }
 
 
+
 function currtentTime() {
-    for (let i = 24; i > -1 ; i--) {
+    var timeBlocks= $(".colorCode")
+    // console.log(timeBlocks)
+    for (let i = 0; i < timeBlocks.length ; i++) {
+        index = parseInt(timeBlocks[i].id);
+        if (index < hour()) {
+            console.log(index)
+            index.addClass('past');
+        }
+
+
         
-        if ( hour() > i) {
-            test().addClass("past")
-        //   console.log(test())  
-            } 
     } 
 
 }    
 
-
-// //save mechanic 
-
-
-
-
-
-
-
-function test() {
-    for (let index = 0; index < allTime.length; index++) {
-        day = allTime[index].hourOfDay;
-        return day;
-    }
-  
-}
-
-
-//all times
-var allTime = [
-    { hourOfDay: $("#12AM") },
-    { hourOfDay: $("#1AM") },
-    { hourOfDay: $("#2AM") },
-    { hourOfDay: $("#3AM") },
-    { hourOfDay: $("#4AM") },
-    { hourOfDay: $("#5AM") },
-    { hourOfDay: $("#6AM") },
-    { hourOfDay: $("#7AM") },
-    { hourOfDay: $("#8AM") },
-    { hourOfDay: $("#9AM") },
-    { hourOfDay: $("#11AM") },
-    { hourOfDay: $("#12PM") },
-    { hourOfDay: $("#1PM") },
-    { hourOfDay: $("#2PM") },
-    { hourOfDay: $("#3PM") },
-    { hourOfDay: $("#4PM") },
-    { hourOfDay: $("#5PM") },
-    { hourOfDay: $("#6PM") },
-    { hourOfDay: $("#7PM") },
-    { hourOfDay: $("#8PM") },
-    { hourOfDay: $("#9PM") },
-    { hourOfDay: $("#11PM") },
-    
-]
-
-
+currtentTime()
 
 //updating time
 setInterval(function () {
-    test()
+    // test()
     displayTime();
     // minute();
     hour();
-    currtentTime();
+    // currtentTime();
 
 }, 1000);
