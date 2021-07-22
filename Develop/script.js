f08c6db24cbcfcef945a1ce1d9c9601744d3f1b0
Jose Avalos <avaljose@hotmai.com>
2021-07-21 22:22:01 -0500

    finished calander save mechanic, added ReadME

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,4 @@
+var desc = $(".description")
 var today = moment();
 
 //current day
@@ -6,46 +7,54 @@ $("#currentDay").text(today.format("[Today is] dddd, MMM Do"));
 function displayTime() {
     var today = moment();
     $("#currentDay").text(today.format("[Today is] dddd, MMM Do"));
-  
+
 }
 
 //save feature
 
-$('.saveBtn').on('click', function(){
+$('.saveBtn').on('click', function () {
 
-     var value = $(this).siblings(".description").val()
-     var time = $(this).parent().id
+    var value = $(this).siblings(".description").val()
+    var time = $(this).siblings(".description").attr('id')
 
-     console.log(value)
-     console.log(time)
-     localStorage.setItem(time, value)
+    console.log(value)
+    console.log(time);
+    localStorage.setItem(time, value)
 
-    })
-// for (let index = 0; index < array.length; index++) {
-//     var displayEvent = ".description " + 
-//     $('#hour-0 .description').val(localStorage.getItem("hour-0"))
-// }
-    $('#hour-0 .description').val(localStorage.getItem("hour-0"))
+})
+function displayEvent() {
+    for (let i = 0; i < desc.length; i++) {
+        var hourOfDay = $(desc[i]).attr('id');
+        
+        console.log(hourOfDay);
+
+        var events = "#" + hourOfDay
+        // console.log(events);
+
+        $(events).val(localStorage.getItem(hourOfDay))
+    }
+}
+// $('#hour-0 .description').val(localStorage.getItem("hour-0"))
+
+displayEvent()
 
 
 
 
 
 
-
-
-function hour(){
+function hour() {
     today = moment().format("H");
     time = parseInt(today);
     return time
 }
 
 function currtentTime() {
-    var timeBlocks= $(".colorCode")
+    var timeBlocks = $(".colorCode")
     // console.log(timeBlocks)
-    for (let i = 0; i < timeBlocks.length ; i++) {
+    for (let i = 0; i < timeBlocks.length; i++) {
         index = parseInt(timeBlocks[i].id);
-        
+
         if (index < hour()) {
             // console.log("past", index)
             $(timeBlocks[i]).addClass('past');
@@ -55,9 +64,9 @@ function currtentTime() {
         } else if (index > hour()) {
             // console.log("future", index)
             $(timeBlocks[i]).addClass('future');
-        }        
-    } 
-}    
+        }
+    }
+}
 
 currtentTime()
 
